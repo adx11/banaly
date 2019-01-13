88d735aeca3f9d3fc5ec6bbc5845c37cf456edae
Anthony DeStefano <adx@fastmail.fm>
2019-01-13 15:53:17 -0500

    Simpler fix for bfd.h

--- loader/loader.cc
+++ loader/loader.cc
@@ -1,24 +1,12 @@
 #include <stdio.h>
+#include <string>
 
-#ifndef PACKAGE
-    #define PACKAGE
-    #ifndef PACKAGE_VERSION
-        #define PACKAGE_VERSION
-        #include <bfd.h>
-        #undef PACKAGE_VERSION
-    #else
-        #include <bfd.h>
-    #endif
-    #undef PACKAGE
-  #else
-    #ifndef PACKAGE_VERSION
-        #define PACKAGE_VERSION
-        #include <bfd.h>
-        #undef PACKAGE_VERSION
-    #else
-        #include <bfd.h>
-    #endif
-  #endif
+// libbfd's authors are a pita
+#define PACKAGE
+#define PACKAGE_VERSION
+#include <bfd.h>
+#undef PACKAGE_VERSION
+#undef PACKAGE
 
 #include "loader.h"
 
